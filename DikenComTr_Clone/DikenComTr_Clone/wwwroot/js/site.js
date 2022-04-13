daf1597e677b7bfa61af38d0982a00bffc872c21
QijiBK <66331527+QijiBK@users.noreply.github.com>
2022-04-13 19:59:36 +0300

    13.04

--- DikenComTr_Clone/DikenComTr_Clone/wwwroot/js/site.js
+++ DikenComTr_Clone/DikenComTr_Clone/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿////$(() => {
+////    $("#menu-btn").click(function () {
+////        $(".nav-content").toggle("fast");
+////    })
+////})
+    
 
-// Write your JavaScript code.
+
+
+
+
+    
