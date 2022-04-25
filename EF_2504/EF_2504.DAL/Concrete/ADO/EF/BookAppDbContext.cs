0dbdcb155a59e13f75e08f5219c50a86b52e6031
QijiBK <66331527+QijiBK@users.noreply.github.com>
2022-04-26 00:18:17 +0300

    2504

---
+++ EF_2504/EF_2504.DAL/Concrete/ADO/EF/BookAppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using EF_2504.DAL.Concrete.ADO.EF.Config;
+using EF_2504.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_2504.DAL.Concrete.ADO.EF
+{
+    public class BookAppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookDetail> BookDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=MSI\\SQLEXPRESS01;Database=BookAppDbAksam;Trusted_Connection=true;");
+
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryConfig());
+        }
+    }
+}
---
+++ EF_2504/EF_2504.DAL/Entities/BookDetail.cs
@@ -0,0 +1,12 @@
+﻿namespace EF_2504.DAL.Entities
+{
+    public class BookDetail
+    {
+        public int BookDetailId { get; set; }
+        public string BookDetailIsbn { get; set; }
+        public string BookDetailCity { get; set; }
+        public string BookDetailCountry { get; set; }
+        public int BookDetailYear { get; set; }
+
+    }
+}
